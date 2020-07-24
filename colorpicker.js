2618039fced4c5a2620a33d4d09393bc77b3dee3
DarenOwl <DarenOwl@users.noreply.github.com>
2020-07-24 12:43:44 +0500

    Colorpicker

--- colorpicker.js
+++ colorpicker.js
@@ -21,19 +21,65 @@ class ColorPicker {
             div.appendChild(colordiv);
         }
 
-        //создаем крестик
-        var icon = document.createElement("i");
-        icon.setAttribute("class","fa fa-times");
-        icon.setAttribute("style","color:white; cursor: pointer;")
-        icon.onclick = () => {ColorPicker.Close()};
-        div.appendChild(icon);
+        //создаем иконку палитры
+        var wheel = document.createElement("img");
+        wheel.src = "img/colorwheel.svg";
+        wheel.setAttribute("class","color");
+        wheel.onclick = () => {ColorPicker.OpenJaames(x,y,consumer)}
+        div.appendChild(wheel);
+
+        //создаем крестик        
+        div.appendChild(this.GetCloseBtn());
 
         document.body.appendChild(div);
     }
+
     static Close(){
         var div = document.getElementById("color-picker");
         if (div != undefined && div != null){
             div.remove();
         }
+    }
+
+    static OpenJaames(x,y,consumer){
+        this.Close();
+
+        var div = document.createElement("div");
+        div.setAttribute("id","color-picker");
+        div.setAttribute("style","top: " + (y - 100) + "px; right: 4%;");
+        document.body.appendChild(div);
+
+        var colorWheel = new iro.ColorPicker("#color-picker", {
+            width: "200",
+            layout: [
+                { 
+                    component: iro.ui.Wheel,
+                    options: {
+                        wheelLightness: true,
+                        wheelAngle: 0,
+                        wheelDirection: "anticlockwise"
+                } 
+                },
+                {
+                    component: iro.ui.Slider,
+                    options: { sliderType: "value" }
+                }
+            ]
+          
+        });
+        colorWheel.on('color:change', function(color) {
+            if (color.index === 0) {
+              consumer(color.hexString);
+            }
+          });      
+        div.appendChild(this.GetCloseBtn());    
+    }
+    
+    static GetCloseBtn(){
+        var icon = document.createElement("i");
+        icon.setAttribute("class","fa fa-times");
+        icon.setAttribute("style","color:white; cursor: pointer;")
+        icon.onclick = () => {ColorPicker.Close()};
+        return icon;
     }
 }
